686fcbf6bd353f48d6afbffbcd586664c69afa88
jhwmac <jkr2505@naver.com>
2024-09-12 17:00:43 +0900

    PCP-51 : JobMainCard, JobSubCard 컴포넌트 구현

--- src/components/pages/onboarding/Onboarding.tsx
+++ src/components/pages/onboarding/Onboarding.tsx
@@ -1,3 +1,5 @@
+import JobMainCard from '@/components/common/ui/card/JobMainCard';
+import JobSubCard from '@/components/common/ui/card/JobSubCard';
 import Radio from '@/components/common/ui/radio/Radio';
 import styled from 'styled-components';
 
@@ -5,7 +7,16 @@ const Onboarding = () => {
   return (
     <Wrapper>
       <Radio id="a" name="a" $size="lg" />
-      <Radio id="a" name="a" />
+      <Radio id="b" name="a" />
+      <Radio id="c" name="a" $size="sm" />
+      <Radio id="b" name="a" />
+      <Radio id="a" name="a" $size="lg" />
+      <JobMainCard text="개발" icon="/assets/icons/graphic/glass/laptop-on.svg" />
+      <JobMainCard text="개발" icon="/assets/icons/graphic/glass/laptop-on.svg" state="selected" />
+      <JobMainCard text="개발" icon="/assets/icons/graphic/glass/laptop-on.svg" state="disabled" />
+      <JobSubCard text="개발" />
+      <JobSubCard text="개발" state="selected" />
+      <JobSubCard text="개발" state="disabled" />
     </Wrapper>
   );
 };
