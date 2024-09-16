e6677e449fad52759af8e1ed12e38f34f6f52b25
bominni <gbm4655@naver.com>
2024-09-17 00:05:29 +0900

    PCP-15: google 로그인 구현

--- src/components/pages/login/GoogleLoginButton.tsx
+++ src/components/pages/login/GoogleLoginButton.tsx
@@ -1,8 +1,25 @@
 import Button from '@/components/common/ui/button/Button';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 const GoogleLoginButton = () => {
+  const handlerGoogleLogin = useGoogleLogin({
+    onSuccess: async (res) => {
+      console.log(res.access_token);
+      await axios({
+        method: 'post',
+        url: '',
+        data: { access_token: res.access_token },
+      })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    },
+  });
+
   return (
-    <Button $type="outline" $size="48" $leftIcon="/assets/icons/line/google.svg">
+    <Button $type="outline" $size="48" $leftIcon="/assets/icons/line/google.svg" onClick={() => handlerGoogleLogin()}>
       구글로 계속하기
     </Button>
   );
