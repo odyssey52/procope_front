d888f1297eb1351ea864f17211c69615d2178514
jhwmac <jkr2505@naver.com>
2024-11-07 13:31:32 +0900

    PCP-97 : ESLint install & airbnb setting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['airbnb'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {},
+};
