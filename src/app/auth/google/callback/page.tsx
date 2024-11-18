9630904f7a4c0ba54b5fd1ef4266c3cc43867fd2
bominni <gbm4655@naver.com>
2024-11-18 18:02:33 +0900

    PCP-15: googleLogin api 연결

---
+++ src/app/auth/google/callback/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import GoogleCallback from "@/components/pages/auth/google/GoogleCallback";
+
+const page = () => <GoogleCallback />;
+
+page.displayName = 'page';
+
+export default page;
