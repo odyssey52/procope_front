1d86c6aa607ade18a53edb2a39587018d071030e
jhwmac <jkr2505@naver.com>
2024-12-12 14:37:11 +0900

    PCP-104 : select 컴포넌트 label description state 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import Description from '@/components/common/ui/description/Description';
+import Label from '@/components/common/ui/label/Label';
 import Select from '@/components/common/ui/select/Select';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const page = () => {
   const [value, setValue] = useState<string>('');
+  const [isError, setIsError] = useState<boolean>(false);
   const valueHandler = (value: string) => {
     setValue(value);
   };
@@ -15,9 +18,26 @@ const page = () => {
     { value: 'option4' },
     { value: 'option5' },
   ];
+  useEffect(() => {
+    if (value === '') {
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
+  }, [value]);
   return (
-    <div>
-      <Select placeholder="선택 필수" value={value} valueHandler={valueHandler} selectOptionList={selectOptionList} />
+    <div style={{ padding: '2%' }}>
+      <Select
+        label={<Label text="label" required />}
+        placeholder="선택 필수"
+        value={value}
+        valueHandler={valueHandler}
+        state={isError ? 'error' : undefined}
+        selectOptionList={selectOptionList}
+        description={
+          <Description text={isError ? '필수 선택입니다' : '선택 완료'} type={isError ? 'error' : 'success'} />
+        }
+      />
     </div>
   );
 };
--- src/components/common/ui/select/Select.tsx
+++ src/components/common/ui/select/Select.tsx
@@ -1,18 +1,29 @@
 import { IconDirectionDown, IconDirectionUp } from '@/assets/icons/line';
+import { theme } from '@/styles/theme';
 import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Text from '../Text';
 import ItemList, { SelectOptionList } from './ItemList';
 
 interface SelectProps {
   label?: React.ReactNode;
+  description?: React.ReactNode;
+  state?: 'default' | 'error' | 'disabled';
   placeholder: string;
   value: string;
   valueHandler: (value: string) => void;
   selectOptionList: SelectOptionList;
 }
 
-const Select = ({ label, placeholder, value, valueHandler, selectOptionList }: SelectProps) => {
+const Select = ({
+  label,
+  placeholder,
+  state = 'default',
+  description,
+  value,
+  valueHandler,
+  selectOptionList,
+}: SelectProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const selectRef = useRef<HTMLDivElement>(null);
 
@@ -40,32 +51,68 @@ const Select = ({ label, placeholder, value, valueHandler, selectOptionList }: S
   return (
     <Wrapper ref={selectRef}>
       {label && label}
-      <SelectBox onClick={toggleOpen}>
-        {placeholder && !value && (
-          <Text variant="body_14_regular" color="tertiary">
-            {placeholder}
-          </Text>
+      <SelectBoxWrapper>
+        <SelectBox onClick={toggleOpen} $state={state} disabled={state === 'disabled'}>
+          {placeholder && !value && (
+            <Text variant="body_14_regular" color="tertiary">
+              {placeholder}
+            </Text>
+          )}
+          {value && (
+            <Text variant="body_14_regular" color="primary">
+              {value}
+            </Text>
+          )}
+          {isOpen ? <IconDirectionUp /> : <IconDirectionDown />}
+        </SelectBox>
+        {isOpen && (
+          <ItemListWrapper>
+            <ItemList selectOptionList={selectOptionList} value={value} valueHandler={onClickItem} />
+          </ItemListWrapper>
         )}
-        {value && (
-          <Text variant="body_14_regular" color="primary">
-            {value}
-          </Text>
-        )}
-        {isOpen ? <IconDirectionUp /> : <IconDirectionDown />}
-      </SelectBox>
-      {isOpen && (
-        <ItemListWrapper>
-          <ItemList selectOptionList={selectOptionList} value={value} valueHandler={onClickItem} />
-        </ItemListWrapper>
-      )}
+      </SelectBoxWrapper>
+      {description && description}
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   position: relative;
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  gap: 8px;
 `;
-const SelectBox = styled.button`
+
+const getSelectBoxStateStyle = {
+  default: css`
+    border: 1px solid ${theme.sementicColors.border.primary};
+    background: ${theme.sementicColors.bg.invers};
+    &:hover {
+      border: 1px solid ${theme.sementicColors.border.primary_hover_pressed};
+      background: ${theme.sementicColors.bg.invers};
+      box-shadow:
+        0px 1px 2px 0px rgba(0, 0, 0, 0.16),
+        0px 0px 1px 0px rgba(0, 0, 0, 0.12);
+    }
+  `,
+  error: css`
+    border: 1px solid ${theme.sementicColors.border.danger};
+    background: ${theme.sementicColors.bg.invers};
+    box-shadow: 0px 0px 0px 2px rgba(248, 113, 113, 0.4);
+  `,
+  disabled: css`
+    &:hover {
+      border: 1px solid ${theme.sementicColors.border.disabled};
+      box-shadow: none;
+    }
+    border: 1px solid ${theme.sementicColors.border.disabled};
+    cursor: not-allowed;
+    opacity: 0.4;
+    background-color: ${theme.sementicColors.bg.disabled};
+  `,
+};
+const SelectBox = styled.button<{ $state: 'default' | 'error' | 'disabled' }>`
   position: relative;
   display: flex;
   justify-content: space-between;
@@ -73,13 +120,18 @@ const SelectBox = styled.button`
   min-width: 242px;
   padding: 8px 12px;
   border-radius: 8px;
-  border: 1px solid ${({ theme }) => theme.sementicColors.border.primary};
-  background: ${({ theme }) => theme.sementicColors.bg.invers};
+  ${({ $state }) => getSelectBoxStateStyle[$state]}
+`;
+const SelectBoxWrapper = styled.div`
+  position: relative;
 `;
 const ItemListWrapper = styled.div`
   position: absolute;
   display: flex;
   top: calc(100% + 2px);
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1;
 `;
 
 Select.displayName = 'Select';
