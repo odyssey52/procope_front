294abf21457385295571deb087b53b3de66092c8
jinhyunwoo-FE <jkr2505@naver.com>
2025-02-16 16:01:32 +0900

    PCP-174 : draghandler editable state binding

--- src/components/common/ui/tiptap/Tiptap.tsx
+++ src/components/common/ui/tiptap/Tiptap.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 const Tiptap = ({ editor }: { editor: Editor }) => {
   return (
     <Wrapper>
-      <DragHandle editor={editor}>
+      <DragHandle editor={editor} className={editor?.isEditable ? 'block' : 'hidden'}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
         </svg>
@@ -18,7 +18,9 @@ const Tiptap = ({ editor }: { editor: Editor }) => {
 };
 
 const Wrapper = styled.div`
+  position: relative;
   .ProseMirror {
+    position: relative;
     &:first-child {
       margin-top: 0;
     }
