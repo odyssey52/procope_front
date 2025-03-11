56f084681072db86d20c1628e59fffbaac704023
jinhyunwoo-FE <jkr2505@naver.com>
2025-03-11 10:08:18 +0900

    PCP-196: 테스트코드 도입을 위한 jest 셋업

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+import '@testing-library/jest-dom';
+
+// 필요한 경우 여기에 전역 테스트 설정을 추가할 수 있습니다.
