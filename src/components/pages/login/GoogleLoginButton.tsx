e68425cdb001fbf9d251abea3d1b1dfba95e1233
jinhyunwoo-FE <jkr2505@naver.com>
2025-03-18 09:54:30 +0900

    PCP-201: google Oauth 자동 로그인 해제 로그인 폼 재 출력하도록 변경

--- src/components/pages/login/GoogleLoginButton.tsx
+++ src/components/pages/login/GoogleLoginButton.tsx
@@ -6,7 +6,7 @@ const GoogleLoginButton = () => {
     const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
     const REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
 
-    const googleLoginUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile`;
+    const googleLoginUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&prompt=select_account`;
 
     window.location.href = googleLoginUrl;
   };
