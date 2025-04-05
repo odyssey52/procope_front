6cd1e99c3cd945ccd7e601c61141d8580edd3903
jinhyunwoo-FE <jkr2505@naver.com>
2025-04-05 16:34:26 +0900

    PCP-216: invite 페이지 team 하위로 이동

--- src/app/invite/[code]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { AuthGuard } from '@/components/common/authguard/AuthGuard';
-import { ReactNode } from 'react';
-
-const layout = ({ children }: { children: ReactNode }) => <AuthGuard>{children}</AuthGuard>;
-
-export default layout;
