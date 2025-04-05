8bc39230fabe00402e581c9e709cfd49e0eee79e
jinhyunwoo-FE <jkr2505@naver.com>
2025-04-05 17:13:30 +0900

    PCP-216: 초대 링크를 통한 팀 합류 기능 구현

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 
 const nextConfig = {
+  reactStrictMode: false,
   compiler: {
     styledComponents: true,
   },
--- src/components/pages/team/invite/InvitePage.tsx
+++ src/components/pages/team/invite/InvitePage.tsx
@@ -1,5 +1,38 @@
+'use client';
+
+import { createInviteTeam } from '@/services/team/teamService';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { toastActions } from '@/store/modal/toast';
+
 const InvitePage = ({ code }: { code: string }) => {
-  return <div>{code}</div>;
+  const router = useRouter();
+
+  const createInviteTeamMutation = useMutation({
+    mutationFn: createInviteTeam,
+    onSuccess: () => {
+      toastActions.open({
+        state: 'success',
+        title: '팀에 성공적으로 합류했습니다.',
+      });
+      router.push('/team');
+    },
+    onError: () => {
+      toastActions.open({
+        state: 'error',
+        title: '팀 합류에 실패했습니다.',
+        description: '초대 링크가 유효하지 않거나 만료되었습니다.',
+      });
+      router.push('/team');
+    },
+  });
+
+  useEffect(() => {
+    createInviteTeamMutation.mutate({ url: code });
+  }, [code]);
+
+  return <div>팀에 합류 중입니다...</div>;
 };
 
 InvitePage.displayName = 'InvitePage';
