11bb22ba6cbc85d266f0cc0a01e08a927c52e93a
jinhyunwoo-FE <jkr2505@naver.com>
2025-05-17 14:13:19 +0900

    PCP-234 : ItemList 노출 항목 최대 8개로 제한 및 스크롤 추가

--- src/shared/ui/select/ItemList.tsx
+++ src/shared/ui/select/ItemList.tsx
@@ -32,16 +32,30 @@ const ItemList = ({ selectOptionList, value, valueHandler }: ItemListProps) => {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  overflow: hidden;
+  overflow-y: auto;
   gap: 2px;
   border-radius: 8px;
   justify-content: space-around;
   border: 1px solid ${({ theme }) => theme.sementicColors.border.primary};
   background: ${({ theme }) => theme.sementicColors.bg.inverse};
+  max-height: calc(40px * 8); // SelectOption의 높이(40px) * 8개
 
   box-shadow:
     0px 2px 4px 0px rgba(0, 0, 0, 0.16),
     0px 0px 2px 0px rgba(0, 0, 0, 0.12);
+
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.sementicColors.border.primary};
+    border-radius: 4px;
+  }
 `;
 
 ItemList.displayName = 'ItemList';
