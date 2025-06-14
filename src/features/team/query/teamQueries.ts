132d24b5f52697fd5ef770fb9f3a3fc180bdab1d
jhwmac <jkr2505@naver.com>
2025-06-14 12:14:32 +0900

    PCP-186 : readTeamRoleCount query 추가

--- src/features/team/query/teamQueries.ts
+++ src/features/team/query/teamQueries.ts
@@ -1,4 +1,4 @@
-import { readTeamList, readTeamDetail, readRetroList } from '@/features/team/services/teamService';
+import { readTeamList, readTeamDetail, readRetroList, readTeamRoleCount } from '@/features/team/services/teamService';
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 import * as types from '@/features/team/services/teamService.type';
 
@@ -14,6 +14,10 @@ const teamQueries = createQueryKeys('team', {
   readRetroList: (params: types.ReadRetroListParams) => ({
     queryKey: [params.teamId],
     queryFn: () => readRetroList(params),
+  }),
+  readTeamRoleCount: (params: types.ReadTeamRoleCountParams) => ({
+    queryKey: [params.role],
+    queryFn: () => readTeamRoleCount(params),
   }),
 });
 
