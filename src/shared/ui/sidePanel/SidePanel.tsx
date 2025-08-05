e2c678492a58e13bcaed0d8958e34ecb514ae8c4
jinhyunwoo-FE <jkr2505@naver.com>
2025-08-05 09:21:22 +0900

    PCP-242: SidePanel motion 추가

--- src/shared/ui/sidePanel/SidePanel.tsx
+++ src/shared/ui/sidePanel/SidePanel.tsx
@@ -3,31 +3,39 @@
 import { IconDirectionRight1 } from '@/shared/assets/icons/line';
 import { sidePanelActions, useSidePanelStore } from '@/shared/store/sidePanel/sidePanel';
 import { elevation, zIndex } from '@/shared/styles/mixin';
+import { AnimatePresence, motion } from 'framer-motion';
 import styled from 'styled-components';
 
 const SidePanel = () => {
   const state = useSidePanelStore();
-  if (!state.isOpen) {
-    return null;
-  }
   return (
-    <Wrapper>
-      <PanelControl>
-        <IconDirectionRight1 onClick={() => sidePanelActions.close()} />
-        {state.moreMenu && state.moreMenu}
-      </PanelControl>
-      {state.content}
-    </Wrapper>
+    <AnimatePresence mode="wait">
+      {state.isOpen && (
+        <Wrapper
+          $isOpen={state.isOpen}
+          initial={{ right: '-100%' }}
+          animate={{ right: 0, transition: { duration: 0.3, ease: 'easeInOut' } }}
+          exit={{ right: '-100%', transition: { duration: 0.3, ease: 'easeInOut' } }}
+        >
+          <PanelControl>
+            <CloseButton onClick={sidePanelActions.close}>
+              <IconDirectionRight1 />
+            </CloseButton>
+            {state.moreMenu && state.moreMenu}
+          </PanelControl>
+          {state.content}
+        </Wrapper>
+      )}
+    </AnimatePresence>
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)<{ $isOpen: boolean }>`
   width: 790px;
   max-width: calc(100vw - 316px);
   padding: 24px 0;
   position: fixed;
-  top: 0;
-  right: 0;
+  top: 55px;
   display: flex;
   flex-direction: column;
   gap: 24px;
@@ -37,12 +45,20 @@ const Wrapper = styled.div`
   min-height: 100vh;
   background-color: ${({ theme }) => theme.sementicColors.bg.inverse};
   border-left: 1px solid ${({ theme }) => theme.sementicColors.border.primary};
+  box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.05);
 `;
 
 const PanelControl = styled.div`
   display: flex;
   padding: 0 24px;
   justify-content: space-between;
+`;
+
+const CloseButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `;
 
 SidePanel.displayName = 'SidePanel';
