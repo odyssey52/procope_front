706d06a4ca6acd10c2a899fbdc2e456a551679e9
jinhyunwoo-FE <jkr2505@naver.com>
2025-08-13 10:29:26 +0900

    PCP-242: SidePanel dom 위치 조정

--- src/shared/ui/provider/Provider.tsx
+++ src/shared/ui/provider/Provider.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import ConfirmDialog from '@/shared/ui/modal/ConfirmModal';
-import Toast from '@/shared/ui/modal/Toast';
 import StyledComponentsRegistry from '@/shared/lib/registry';
 import GlobalStyles from '@/shared/styles/GlobalStyles';
 import { theme } from '@/shared/styles/theme';
+import ConfirmDialog from '@/shared/ui/modal/ConfirmModal';
+import Toast from '@/shared/ui/modal/Toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import SidePanel from '../sidePanel/SidePanel';
 
 export const queryClient = new QueryClient();
 
@@ -29,7 +28,6 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
       <StyledComponentsRegistry>
         <ThemeProvider theme={theme}>
           <GlobalStyles />
-          <SidePanel />
           <Toast />
           <ConfirmDialog />
           {children}
