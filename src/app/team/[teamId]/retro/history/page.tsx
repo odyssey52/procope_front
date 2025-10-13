d43c0b3cb3e7a65fed508f735ad08791ad2c5cba
jinhyunwoo-FE <jkr2505@naver.com>
2025-10-13 11:00:46 +0900

    PCP-301 : SidePanel width 수정

--- src/app/team/[teamId]/retro/history/page.tsx
+++ src/app/team/[teamId]/retro/history/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import RetroHistoryPage from '@/features/team/components/detail/retro/history/RetroHistoryPage';
+import Error from '@/shared/ui/error/Error';
 
 const page = () => {
-  return <RetroHistoryPage />;
+  return <Error title="준비 중" description="추후 업데이트 예정입니다." />;
+  // return <RetroHistoryPage />;
 };
 
 page.displayName = 'page';
--- src/app/team/[teamId]/setting/page.tsx
+++ src/app/team/[teamId]/setting/page.tsx
@@ -1,23 +1,10 @@
 'use client';
 
 import Error from '@/shared/ui/error/Error';
-import styled from 'styled-components';
 
 const page = () => {
-  return (
-    <Wrapper>
-      <Error title="준비 중" description="추후 업데이트 예정입니다." />
-    </Wrapper>
-  );
+  return <Error title="준비 중" description="추후 업데이트 예정입니다." />;
 };
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 80%;
-`;
 
 page.displayName = 'page';
 
