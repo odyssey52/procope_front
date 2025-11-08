68e7ce348fe72cbede2f0eb05fa57735f1fb5fa5
jhwmac <jkr2505@naver.com>
2025-11-08 14:00:49 +0900

    PCP-322 : tiptap undo 기능 추가

--- src/features/team/hooks/useRetroAutoSave.ts
+++ src/features/team/hooks/useRetroAutoSave.ts
@@ -6,6 +6,7 @@ import Document from '@tiptap/extension-document';
 import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
 import Placeholder from '@tiptap/extension-placeholder';
+import History from '@tiptap/extension-history';
 import { useEditor, Editor } from '@tiptap/react';
 import { useEffect, useRef, useState } from 'react';
 
@@ -55,6 +56,7 @@ export function useRetroAutoSave(options: UseRetroAutoSaveOptions): UseRetroAuto
       Paragraph,
       Text,
       BulletList,
+      History,
       OrderedList,
       ListItem,
       Heading.configure({ levels: [1, 2, 3] }),
